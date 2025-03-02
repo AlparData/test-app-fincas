b27d695db63d301fc4f6c554905aeb9e46ebcce9
Santiago Tojo <santiago.tojo@alpardata.com.ar>
2025-03-02 00:56:18 -0300

    fix

--- Fincas/models/work_orders.py
+++ Fincas/models/work_orders.py
@@ -7,18 +7,8 @@ class FincaOrdenTrabajo(models.Model):
     name = fields.Char(string='Referencia', required=True, copy=False, default=lambda self: self.env['ir.sequence'].next_by_code('finca.orden.trabajo'))
     fecha = fields.Date(string='Fecha', required=True, default=fields.Date.context_today)
     responsable_id = fields.Many2one('res.users', string='Responsable', required=True)
-    equipo_ids = fields.Many2many(
-    'hr.employee', 
-    'finca_orden_trabajo_equipo_rel',  # Nombre único para la tabla de relación
-    'orden_id', 'empleado_id', 
-    string='Equipo de Trabajo'
-    )
-    maquinaria_ids = fields.Many2many(
-    'fleet.vehicle', 
-    'finca_orden_trabajo_maquinaria_rel',  # Nombre único para la tabla de relación
-    'orden_id', 'vehiculo_id', 
-    string='Maquinaria Utilizada'
-    )
+    equipo_ids = fields.Many2many('hr.employee', string='Equipo de Trabajo')
+    maquinaria_ids = fields.Many2many('fleet.vehicle', string='Maquinaria Utilizada')
     implementos_ids = fields.Many2many('product.product', string='Implementos')
     productos_aplicados_ids = fields.One2many('finca.producto.aplicado', 'orden_id', string='Productos Aplicados')
     parcela_id = fields.Many2one('finca.parcela', string='Parcela')
