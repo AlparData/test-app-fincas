44d5879f23775e018cf0d37610b152cc28d365b9
Santiago Tojo <santiago.tojo@alpardata.com.ar>
2025-03-02 01:08:44 -0300

    fix

--- Fincas/models/work_orders.py
+++ Fincas/models/work_orders.py
@@ -7,7 +7,6 @@ class FincaOrdenTrabajo(models.Model):
     name = fields.Char(string='Referencia', required=True, copy=False, default=lambda self: self.env['ir.sequence'].next_by_code('finca.orden.trabajo'))
     fecha = fields.Date(string='Fecha', required=True, default=fields.Date.context_today)
     responsable_id = fields.Many2one('res.users', string='Responsable', required=True)
-    maquinaria_ids = fields.Many2many('fleet.vehicle', string='Maquinaria Utilizada')
     implementos_ids = fields.Many2many('product.product', string='Implementos')
     productos_aplicados_ids = fields.One2many('finca.producto.aplicado', 'orden_id', string='Productos Aplicados')
     parcela_id = fields.Many2one('finca.parcela', string='Parcela')
